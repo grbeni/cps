fc99481ed2b630aca2a5de5d25322a3c51a0d8e4
Bence Graics <grbeni@gmail.com>
2017-12-02 22:06:57 +0100

    Project added.

---
+++ hu.bme.mit.cps/src/hu/bme/mit/cps/UvegHazTypeCode.java
@@ -0,0 +1,29 @@
+package hu.bme.mit.cps;
+/*
+WARNING: THIS FILE IS AUTO-GENERATED. DO NOT MODIFY.
+
+This file was generated from .idl using "rtiddsgen".
+The rtiddsgen tool is part of the RTI Connext distribution.
+For more information, type 'rtiddsgen -help' at a command shell
+or consult the RTI Connext manual.
+*/
+
+import com.rti.dds.typecode.*;
+
+public class  UvegHazTypeCode {
+    public static final TypeCode VALUE = getTypeCode();
+
+    private static TypeCode getTypeCode() {
+        TypeCode tc = null;
+        int __i=0;
+        StructMember sm[]=new StructMember[3];
+
+        sm[__i]=new  StructMember("ID", false, (short)-1,  false,(TypeCode) new TypeCode(TCKind.TK_STRING,255),0 , false);__i++;
+        sm[__i]=new  StructMember("Value", false, (short)-1,  false,(TypeCode) TypeCode.TC_DOUBLE,1 , false);__i++;
+        sm[__i]=new  StructMember("TimeStamp", false, (short)-1,  false,(TypeCode) TypeCode.TC_LONG,2 , false);__i++;
+
+        tc = TypeCodeFactory.TheTypeCodeFactory.create_struct_tc("UvegHaz",ExtensibilityKind.EXTENSIBLE_EXTENSIBILITY,  sm);        
+        return tc;
+    }
+}
+
