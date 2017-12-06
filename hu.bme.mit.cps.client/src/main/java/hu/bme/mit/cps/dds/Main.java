f34754a9db21738e2886ecbad4005de4b8058c48
Bence Graics <grbeni@gmail.com>
2017-12-06 21:10:55 +0100

    Refactor.

--- hu.bme.mit.cps.client/src/main/java/hu/bme/mit/cps/dds/Main.java
+++ hu.bme.mit.cps.client/src/main/java/hu/bme/mit/cps/dds/Main.java
@@ -8,47 +8,19 @@ import com.rti.dds.publication.Publisher;
 import com.rti.dds.subscription.Subscriber;
 import com.rti.dds.topic.Topic;
 
-public class MyPublisher {
+public class Main {
 	
-	private static boolean shutdownFlag = false;
+	private static UvegHazDataWriter dataWriter;
+	private static UvegHazDataReader dataReader;
+	private static DomainParticipant participant;
 	
 	public static void main(String[] args) {		
-		DomainParticipant participant = DomainParticipantFactory.get_instance().create_participant(
-				0, DomainParticipantFactory.PARTICIPANT_QOS_DEFAULT, null, StatusKind.STATUS_MASK_NONE);
-		if (participant == null) {
-			System.err.println("Unable to create domain participant.");
-			return;
-		}
+		// Creating the infrastructure
+		createDdsInfrastructure();		
 		
-		String uvegHazTypeName = UvegHazTypeSupport.get_type_name();
-		UvegHazTypeSupport.register_type(participant, uvegHazTypeName);
-		
-		Topic topic = participant.create_topic("UvegHaz", uvegHazTypeName,
-				DomainParticipant.TOPIC_QOS_DEFAULT, null, StatusKind.STATUS_MASK_NONE);
-		if (topic == null) {
-			System.err.println("Unable to create topic.");
-			return;
-		}
-		// Creating a writer
-		UvegHazDataWriter dataWriter = (UvegHazDataWriter) participant.create_datawriter(
-				topic, Publisher.DATAWRITER_QOS_USE_TOPIC_QOS, null, StatusKind.DATA_AVAILABLE_STATUS);
-		if (dataWriter == null) {
-			System.err.println("Unable to create DDS writer.");
-			return;
-		}
-		// Creating a reader
-		UvegHazDataReader dataReader = (UvegHazDataReader) participant.create_datareader(topic,
-				Subscriber.DATAREADER_QOS_USE_TOPIC_QOS, new MySubscriber(), StatusKind.DATA_AVAILABLE_STATUS);
-		if (dataReader == null) {
-			System.err.println("Unable to create DDS reader.");
-			return;
-		}
-		
-		System.out.println("Ready to write data.");
-		System.out.println("Press Ctrl + C to terminate.");
 		// Writing data 
 		try {
-			Thread.sleep(10000);
+			Thread.sleep(5000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
@@ -57,9 +29,43 @@ public class MyPublisher {
 		dataWriter.write(uvegHaz, InstanceHandle_t.HANDLE_NIL);
 
 		System.out.println("Terminating.");
+		tearDownDdsInfrastructure();
+	}
+	
+	private static void createDdsInfrastructure() {
+		participant = DomainParticipantFactory.get_instance().create_participant(
+				0, DomainParticipantFactory.PARTICIPANT_QOS_DEFAULT, null, StatusKind.STATUS_MASK_NONE);
+		if (participant == null) {
+			System.err.println("Unable to create domain participant.");
+			return;
+		}
+		// Registering our own type
+		String uvegHazTypeName = UvegHazTypeSupport.get_type_name();
+		UvegHazTypeSupport.register_type(participant, uvegHazTypeName);
+		// Creating the topic
+		Topic topic = participant.create_topic("UvegHaz", uvegHazTypeName,
+				DomainParticipant.TOPIC_QOS_DEFAULT, null, StatusKind.STATUS_MASK_NONE);
+		if (topic == null) {
+			System.err.println("Unable to create topic.");
+			return;
+		}
+		dataWriter = (UvegHazDataWriter) participant.create_datawriter(
+				topic, Publisher.DATAWRITER_QOS_USE_TOPIC_QOS, null, StatusKind.DATA_AVAILABLE_STATUS);
+		if (dataWriter == null) {
+			System.err.println("Unable to create DDS writer.");
+			return;
+		}
+		dataReader = (UvegHazDataReader) participant.create_datareader(topic,
+				Subscriber.DATAREADER_QOS_USE_TOPIC_QOS, new MySubscriber(), StatusKind.DATA_AVAILABLE_STATUS);
+		if (dataReader == null) {
+			System.err.println("Unable to create DDS reader.");
+			return;
+		}
+	}
+	
+	private static void tearDownDdsInfrastructure() {
 		participant.delete_contained_entities();
-		DomainParticipantFactory.get_instance().delete_participant(participant);	
-		
+		DomainParticipantFactory.get_instance().delete_participant(participant);		
 	}
 
 	
--- hu.bme.mit.cps.client/src/main/java/hu/bme/mit/cps/dds/MySubscriber.java
+++ hu.bme.mit.cps.client/src/main/java/hu/bme/mit/cps/dds/MySubscriber.java
@@ -1,61 +1,12 @@
 package hu.bme.mit.cps.dds;
 
-import com.rti.dds.domain.DomainParticipant;
-import com.rti.dds.domain.DomainParticipantFactory;
 import com.rti.dds.infrastructure.RETCODE_ERROR;
 import com.rti.dds.infrastructure.RETCODE_NO_DATA;
-import com.rti.dds.infrastructure.StatusKind;
 import com.rti.dds.subscription.DataReader;
 import com.rti.dds.subscription.DataReaderAdapter;
 import com.rti.dds.subscription.SampleInfo;
-import com.rti.dds.subscription.Subscriber;
-import com.rti.dds.topic.Topic;
 
 public class MySubscriber extends DataReaderAdapter {
-
-	private static boolean shutdownFlag = false;
-
-	public static void main(String[] args) {
-		DomainParticipant participant = DomainParticipantFactory.get_instance().create_participant(0,
-				DomainParticipantFactory.PARTICIPANT_QOS_DEFAULT, null, StatusKind.STATUS_MASK_NONE);
-		if (participant == null) {
-			System.err.println("Unable to create domain participant.");
-			return;
-		}
-		
-		String uvegHazTypeName = UvegHazTypeSupport.get_type_name();
-		UvegHazTypeSupport.register_type(participant, uvegHazTypeName);
-
-		Topic topic = participant.create_topic("UvegHaz", uvegHazTypeName,
-				DomainParticipant.TOPIC_QOS_DEFAULT, null, StatusKind.STATUS_MASK_NONE);
-		if (topic == null) {
-			System.err.println("Unable to create topic.");
-			return;
-		}
-
-		UvegHazDataReader dataReader = (UvegHazDataReader) participant.create_datareader(topic,
-				Subscriber.DATAREADER_QOS_USE_TOPIC_QOS, new MySubscriber(), StatusKind.DATA_AVAILABLE_STATUS);
-		if (dataReader == null) {
-			System.err.println("Unable to create DDS reader.");
-			return;
-		}
-
-		System.out.println("Ready to read data.");
-		System.out.println("Press Ctrl + C to terminate.");
-
-		while (!shutdownFlag) {
-			try {
-				Thread.sleep(2000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-
-		System.out.println("Terminating.");
-		participant.delete_contained_entities();
-		DomainParticipantFactory.get_instance().delete_participant(participant);
-
-	}
 
 	public void on_data_available(DataReader reader) {
 		UvegHazDataReader uvegHazReader = (UvegHazDataReader) reader;
@@ -66,11 +17,8 @@ public class MySubscriber extends DataReaderAdapter {
 				UvegHaz given = new UvegHaz();
 				uvegHazReader.take_next_sample(given, info);
 				if (info.valid_data) {
-					System.out.println("LOLGEC");
+					System.out.println("Data arrived:");
 					System.out.println(given.Value);
-					if (given.Value == -1.0) {
-						shutdownFlag = true;
-					}
 				}
 			} catch (RETCODE_NO_DATA e) {
 				break;
